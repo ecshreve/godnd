cc3e6681b66976b1e1d756231db7684db6b6a55d
Eric Shreve <ecshreve13@gmail.com>
2020-10-30 20:53:35 -0700

    api: move basic api funcs to a separate file

---
+++ pkg/api/endpoints.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/samsarahq/go/oops"
+)
+
+func (c *Client) GetAbilityScores(ctx context.Context) (*ResourceList, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ability-scores", c.BaseURL), nil)
+	if err != nil {
+		return nil, oops.Wrapf(err, "unable to build request")
+	}
+
+	req = req.WithContext(ctx)
+
+	res := ResourceList{}
+	if err := c.sendRequest(req, &res); err != nil {
+		return nil, oops.Wrapf(err, "unable to get ability-scores")
+	}
+
+	return &res, nil
+}
+
+func (c *Client) GetAbilityScoreByIndex(ctx context.Context, index string) (*apiAbilityScore, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ability-scores/%s", c.BaseURL, index), nil)
+	if err != nil {
+		return nil, oops.Wrapf(err, "unable to build request")
+	}
+
+	req = req.WithContext(ctx)
+
+	res := apiAbilityScore{}
+	if err := c.sendRequest(req, &res); err != nil {
+		return nil, oops.Wrapf(err, "unable to get ability score")
+	}
+
+	return &res, nil
+}
--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1,13 +1,5 @@
 package api
 
-import (
-	"context"
-	"fmt"
-	"net/http"
-
-	"github.com/samsarahq/go/oops"
-)
-
 //go:generate genny
 
 // ResourceList is a list of APIReferences for an endpoint.
@@ -21,36 +13,4 @@ type APIReference struct {
 	Index string `json:"index"`
 	Name  string `json:"name"`
 	URL   string `json:"url"`
-}
-
-func (c *Client) GetAbilityScores(ctx context.Context) (*ResourceList, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ability-scores", c.BaseURL), nil)
-	if err != nil {
-		return nil, oops.Wrapf(err, "unable to build request")
-	}
-
-	req = req.WithContext(ctx)
-
-	res := ResourceList{}
-	if err := c.sendRequest(req, &res); err != nil {
-		return nil, oops.Wrapf(err, "unable to get ability-scores")
-	}
-
-	return &res, nil
-}
-
-func (c *Client) GetAbilityScoreByIndex(ctx context.Context, index string) (*apiAbilityScore, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ability-scores/%s", c.BaseURL, index), nil)
-	if err != nil {
-		return nil, oops.Wrapf(err, "unable to build request")
-	}
-
-	req = req.WithContext(ctx)
-
-	res := apiAbilityScore{}
-	if err := c.sendRequest(req, &res); err != nil {
-		return nil, oops.Wrapf(err, "unable to get ability score")
-	}
-
-	return &res, nil
 }
