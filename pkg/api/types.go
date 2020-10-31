72e67948069228a40a96630979f0a4672bade9b1
Eric Shreve <ecshreve13@gmail.com>
2020-10-30 21:44:44 -0700

    api: add types for common models
    These types are the building blocks used to define the other larger data
    types returned from the API.

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -14,3 +14,22 @@ type APIReference struct {
 	Name  string `json:"name"`
 	URL   string `json:"url"`
 }
+
+// Choice is a selection of Choose number of ResourceTypes out of the the From list.
+type Choice struct {
+	Choose       int32          `json:"choose"`
+	ResourceType string         `json:"type"`
+	From         []APIReference `json:"from"`
+}
+
+// Cost is a monetary amount with a Quantity and Unit.
+type Cost struct {
+	Quantity int32  `json:"quantity"`
+	Unit     string `json:"unit"`
+}
+
+// AbilityBonus is a bonus modifier to the associated AbilityScore.
+type AbilityBonus struct {
+	Bonus        int32        `json:"bonus"`
+	AbilityScore APIReference `json:"ability_score"`
+}
