9a37d0315397cd8c38d4e883754fb87b3773eca5
Eric Shreve <ecshreve13@gmail.com>
2020-11-01 23:29:54 -0800

    genny: tweak refact to match old approach

--- cmd/genny/main.go
+++ cmd/genny/main.go
@@ -10,43 +10,34 @@ import (
 func main() {
 	ctx := context.Background()
 
-	// toGen := []string{
-	// 	"ability-scores",
-	// 	"classes",
-	// 	"conditions",
-	// 	"damage-types",
-	// 	"equipment-categories",
-	// 	"equipment",
-	// 	// "features", type name collision with something from the `conditions` endpoint
-	// 	"languages",
-	// 	"magic-items",
-	// 	"magic-schools",
-	// 	// "monsters", this doc includes information on the filter query params, need to handle it
-	// 	"proficiencies",
-	// 	"races",
-	// 	// "rules",
-	// 	// "rule-sections",
-	// 	"skills",
-	// 	// "spellcasting",
-	// 	// "spells",
-	// 	// "starting-equipment",
-	// 	"subclasses",
-	// 	"subraces",
-	// 	"traits",
-	// 	"weapon-properties",
-	// }
-
-	// pathForGenFile := "/Users/ericshreve/github.com/godnd/pkg/api/generated_types.go"
-	// err := genny.GenerateTypes(ctx, toGen, pathForGenFile)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
-	toGenRefactor := []string{
+	toGen := []string{
+		"ability-scores",
+		"classes",
 		"conditions",
+		"damage-types",
+		"equipment-categories",
+		"equipment",
+		// "features", type name collision with something from the `conditions` endpoint
+		"languages",
+		"magic-items",
+		"magic-schools",
+		// "monsters", this doc includes information on the filter query params, need to handle it
+		"proficiencies",
+		"races",
+		// "rules",
+		// "rule-sections",
+		"skills",
+		// "spellcasting",
+		// "spells",
+		// "starting-equipment",
+		"subclasses",
+		"subraces",
+		"traits",
+		"weapon-properties",
 	}
 
-	err := genny.GenerateTypesV2(ctx, toGenRefactor)
+	pathForGenFile := "/Users/ericshreve/github.com/godnd/pkg/api/generated_types.go"
+	err := genny.GenerateTypesV2(ctx, toGen, pathForGenFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
--- internal/genny/consts_test.go
+++ internal/genny/consts_test.go
@@ -146,3 +146,64 @@ const validTableRow = `<tr>
   <td align="left">index field description</td>
   <td align="left">string</td>
 </tr>`
+
+const bugimade = `
+GET api/subraces/{index}</h3>
+
+<pre><code>{
+  "index": "hill-dwarf",
+  "name": "Hill Dwarf",
+  "race": {
+    "name": "Dwarf",
+    "index": "dwarf",
+    "url": "/api/races/dwarf",
+  },
+  "desc": "As a hill dwarf, you have keen senses, deep intuition, and remarkable resilience.",
+  "ability_bonuses": [
+    {
+      "ability_score": {
+        "index": "wis",
+        "name": "WIS",
+        "url": "/api/ability-scores/wis"
+      },
+      "bonus": 1
+    }
+  ],
+  "starting_proficiencies": [],
+  "languages": [],
+  "racial_traits": [
+    {
+      "name": "Dwarven Toughness",
+      "index": "dwarven-toughness",
+      "url": "/api/traits/stonecunning"
+    }
+  ],
+  "url": "/api/subraces/hill-dwarf"
+}</code></pre>
+
+<h4>subrace</h4>
+
+<table>
+  <thead>
+    <tr>
+      <th align="left">Name</th>
+      <th align="left">Description</th>
+      <th align="left">Data Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td align="left">ability_bonuses</td>
+      <td align="left">Additional ability bonuses granted by this sub race.</td>
+      <td align="left">list (<a href="#ability-bonus">AbilityBonus</a>)</td>
+    </tr>
+    <tr>
+      <td align="left">starting_proficiencies</td>
+      <td align="left">Starting proficiencies for all new characters of this subrace.</td>
+      <td align="left">
+        list <a href="#apireference">APIReference</a> (<a href="#proficiencies">Proficiencies</a>)
+      </td>
+    </tr>
+  </tbody>
+</table>
+`
