e329c5188b095db8a0d103369643d9ec82f02a45
shreve <eric@shreve.dev>
2024-11-23 01:25:39 -0800

    feat: clean up repo for new version

--- cmd/godnd/main.go
+++ cmd/godnd/main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"context"
-	"log"
-
-	"github.com/ecshreve/godnd/pkg/api"
-	"github.com/kr/pretty"
-)
+import "fmt"
 
 func main() {
-	cc := api.NewClient()
-	ctx := context.Background()
-	r, err := cc.ConditionByIndex(ctx, "blinded")
-	//r, err := cc.ConditionAll(context.Background())
-	//r, err := cc.AbilityScoreByIndex(ctx, "str")
-	//r, err := cc.AbilityScoreAll(ctx)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pretty.Print(r)
+	fmt.Println("Hello, World!")
 }
